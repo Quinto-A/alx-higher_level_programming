24aad7d4abbf3c609acc933da2528599f19b5f03
Acquino Quinto <acquinoquinto5@gmail.com>
2024-01-23 12:40:48 -0800

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    """ prints an integer.
+
+    Args:
+        Value (int): integer to be printed.
+
+    Returns:
+        True if value has been correctly printed
+    """
+    try:
+        print("{.d}".format(value))
+        return (True)
+    except(TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
