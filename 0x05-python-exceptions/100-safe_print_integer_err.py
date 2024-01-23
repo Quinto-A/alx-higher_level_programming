7c4eb9ebb76fd77baae740e2b4ce8907fc1aadd5
Acquino Quinto <acquinoquinto5@gmail.com>
2024-01-23 12:45:12 -0800

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 def safe_print_integer_err(value):
     """ prints an integer.
 
@@ -10,7 +12,7 @@ def safe_print_integer_err(value):
         True if value has been correctly printed
     """
     try:
-        print("{.d}".format(value))
+        print("{:d}".format(value))
         return (True)
     except(TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
