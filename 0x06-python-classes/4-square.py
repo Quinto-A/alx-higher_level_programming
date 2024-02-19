2c34364a364bf4ef01d12ee8d257eb08ee0996e0
Acquino Quinto <acquinoquinto5@gmail.com>
2024-02-18 19:31:16 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,7 +1,32 @@
 #!/usr/bin/python3
-"""defines a square"""
+
+"""Define a class square"""
+
 
 class Square:
-    """represents a square"""
-    def __init__(self, size = 0)
-    
+    """Represents a square"""
+
+    def __init__(self, size = 0):
+        """Initializes a new square.
+
+        Args:
+            size (int): new square size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area of current square"""
+
+        return (self.__size * self.__size)
