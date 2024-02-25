a94f51e4d698a596d3d235bb2222816d4e89caa3
Quinto <acquinoquinto5@gmail.com>
2024-02-25 10:53:40 +0300

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """adds two integers"""
 
-    def add_integer(a, b=98):
-        """adds int a, b.
-        Arguments:
-            a(int): value to be added.
-            b(int): value to be added
-            return a + b.
-        >>> add_integer(3, 98)
-            101
-        """
-
+def add_integer(a, b=98):
+	"""adds int a, b.
+	float arguments are typecasted into int before addition is perfomed
+	Raises:
+		TypeError: if either a or b ia a non-integer or non-float
+	"""
+	if((not isinstance(a, int) and not isinstance(a, float))):
+		raise TypeError("a must be an integer or b must be an integer")
+	if ((not isinstance(b, int) and not isinstance(b, float))):
+		raise TypeError("b must be an integer")
+	return(int(a) + int(b))
