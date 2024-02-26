c29eca7d08fd5cfce0e2ac76dbd9e5311d7d0751
Acquino Quinto <acquinoquinto5@gmail.com>
2024-02-26 10:57:34 -0800

    tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""adds two integers"""
+
+    def add_integer(a, b=98):
+        """adds int a, b.
+        Arguments:
+            a(int): value to be added.
+            b(int): value to be added
+            return a + b.
+        >>> add_integer(3, 98)
+            101
+        """
+
