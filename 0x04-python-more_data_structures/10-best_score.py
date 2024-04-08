8f0f0f7d32bd3aaef145e7e6efc7b7806905b470
Quinto <acquinoquinto5@gmail.com>
2024-04-09 00:14:45 +0300

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     if a_dictionary:
-        sorted_items = sorted(a_dictionary.items(), key=lambda x: x[1])
-        return (sorted_items[-1])
+        sorted_items = max(a_dictionary, key=a_dictionary.get)
+        return (sorted_items)
     else:
         return (None)
