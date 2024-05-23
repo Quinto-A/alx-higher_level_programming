f90b2ffe1a7e44a58c0d99d845beab8572362e2d
Quinto A <acquinoquinto5@gmail.com>
2024-05-23 23:47:16 +0300

    5-base_geometry.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """determines if an objected is instance of direct or indirect inheritance"""
+    """an object is instance of direct or indirect inheritance"""
     if any(issubclass(type(obj), subclass) for subclass in a_class.__subclasses__()):
         return True
     else:
