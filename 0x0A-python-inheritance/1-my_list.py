6629a5c78f886f570a08e729dccab3611616e683
Quinto A <acquinoquinto5@gmail.com>
2024-05-23 21:49:48 +0300

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,7 @@ class MyList(list):
     """Defines a module that sorts a list"""
 
     def print_sorted(self):
-        """"prints a list in ascending order"""
+        if not self:
+            raise ValueError("Cannot print sorted list: List is empty")
+
         print(sorted(self))
