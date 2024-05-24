bc224cfd0de3ea04a97db1db527e33482649bf71
Quinto A <acquinoquinto5@gmail.com>
2024-05-25 01:09:41 +0300

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a class that inherits from int"""
+
+class MyInt(int):
+    """a class that iherits from int and inverts == and !="""
+
+    def __eq__(self, other):
+        """overide =="""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """override !="""
+        return super().__eq__(other)
