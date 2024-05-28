bf89c31b81a96948649c3e51a5d46e64199f86b5
Quinto A <acquinoquinto5@gmail.com>
2024-05-28 22:19:20 +0300

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, mode="a", encoding="utf-8") as file:
+        return file.write(text)
