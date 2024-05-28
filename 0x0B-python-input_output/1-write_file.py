9a9da16c5d14e8eb2b73f5de1aff64acdf25123e
Quinto A <acquinoquinto5@gmail.com>
2024-05-28 22:03:06 +0300

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """writes a string to a text file and returns character number"""
     with open(filename, mode="w", encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
