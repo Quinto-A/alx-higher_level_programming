2be66f661148e9d89200e003939d935733e381c1
Quinto A <acquinoquinto5@gmail.com>
2024-05-30 21:02:53 +0300

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a function that returns list of lists"""
+
+def pascal_triangle(n):
+    """ returns list of lists of integers"""
+    if n <= 0:
+        return []
+    
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            for j in range(1, i):
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
