ab72b1a8bb49efcdd58127ff541ea90fdd0fce22
Quinto A <acquinoquinto5@gmail.com>
2024-06-01 23:12:04 +0300

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""a class named base"""
+
+class Base:
+    """ forms the base for all other classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """creates a new class instance"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
