20cdd0ef3c70a660c3e0a3cdb8accfd06f661e13
Quinto A <acquinoquinto5@gmail.com>
2024-06-12 17:05:41 +0300

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """a class Rectangle"""
 
 
@@ -96,3 +97,19 @@ class Rectangle(Base):
     def __str__(self):
         """overrides the __str__ method"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if len(args) > 0:
+            self.id = args[0]
+
+        if len(args) > 1:
+            self.width = args[1]
+
+    ¦   if len(args) > 2:
+            self.height = args[2]
+
+    ¦   if len(args) > 3:
+            self.x = args[3]
+
+    ¦   if len(args) > 4:
+            self.y = args[4]
