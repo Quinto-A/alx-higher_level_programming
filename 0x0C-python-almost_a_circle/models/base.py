1e244d9b871b3bd8d7768b5346f023a85d00f5f9
Quinto A <acquinoquinto5@gmail.com>
2024-06-15 16:28:48 +0300

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """"a class named base"""
 
+import json
+
 
 class Base:
     """ forms the base for all other classes"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -13,3 +16,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
