d33079eca38aee4b78c1a79fe2a9bf4affa32bfa
Quinto-A <acquinoquinto5@gmail.com>
2025-01-22 11:23:42 -0800

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends GET request and displays only body of code 200
-[ "$(curl -sL -w "%{http_code}%" -o response_body "$1")" eq 200 ] && cat response_body || echo "Request failed"
+status=$(curl -sL -w "%{http_code}" -o response_body "$1"); [ "$status" = "200" ] && cat response_body || echo "Request failed"
